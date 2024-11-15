c2cffd49c06cbcdc0faf35ec36004ce0a27fc8c4
24HP9459 24HP9459 <24HP9459@admins-iMac.local>
2024-11-15 02:46:12 -0800

    messsage

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+const PORT = 3333;
+
+const app = express();
+
+app.use(express.json());
+const todos = [
+  {
+    id:1,
+    title:"Wake up",
+    checked:false,
+  }
+]
+app.get("/todos", (req, res) => {
+  res.send(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const title =  req.body.title;
+  if (!title) return res.status(400).send({ message: "title is not found" });
+  const newTodo={
+    id: todos[todos.length - 1].id + 1,
+    title: title,
+    checked: false,
+  };
+  todos.push(newTodo);
+  return res.send(newTodo);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
